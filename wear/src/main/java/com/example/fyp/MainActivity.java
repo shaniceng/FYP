0969ad531abd3f53b25cff0c477ffc0708a643a0
shaniceNg <ngshanice1@yahoo.com.sg>
2020-04-22 23:26:01 +0800

    Initial Commit + watch Ui

--- wear/src/main/java/com/example/fyp/MainActivity.java
+++ wear/src/main/java/com/example/fyp/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.fyp;
 
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.input.RotaryEncoder;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.NotificationCompat.WearableExtender;
+
+import static android.provider.CalendarContract.EXTRA_EVENT_ID;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView, currentTime;
     private Button trackActivity, heartRate, stepsCount;
     private Calendar calendar;
+    private ScrollView myView;
 
 
     @Override
@@ -25,12 +35,13 @@ public class MainActivity extends WearableActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mTextView = findViewById(R.id.textView_);
         trackActivity = findViewById(R.id.btnWTrackActivity);
         heartRate = findViewById(R.id.btnWHeartRate);
         stepsCount = findViewById(R.id.btnWStepsCount);
         currentTime=findViewById(R.id.tvCurrentTime);
-        //myView= (ScrollView)  getLayoutInflater().inflate(R.layout.myview,null);
+        myView= (ScrollView) findViewById(R.id.myview);
 
         calendar=Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
@@ -60,27 +71,7 @@ public class MainActivity extends WearableActivity {
         });
 
         Refresh();
-
-             /* myView.setOnGenericMotionListener(new View.OnGenericMotionListener() {
-            @Override
-            public boolean onGenericMotion(View v, MotionEvent ev) {
-                if (ev.getAction() == MotionEvent.ACTION_SCROLL && RotaryEncoder.isFromRotaryEncoder(ev)) {
-                    // Don't forget the negation here
-                    float delta = -RotaryEncoder.getRotaryAxisValue(ev) * RotaryEncoder.getScaledScrollFactor(
-                            getApplicationContext());
-
-                    // Swap these axes if you want to do horizontal scrolling instead
-                    v.scrollBy(0, Math.round(delta));
-
-                    return true;
-                }
-
-                return false;
-            }
-        });*/
-
-
-
+        createNoti();
     }
 
 
@@ -124,5 +115,34 @@ public class MainActivity extends WearableActivity {
         };
         handler.postDelayed(runnable, milliseconds);
     }
+
+    public void createNoti(){
+        int notificationId = 001;
+        // The channel ID of the notification.
+        String id = "my_channel_01";
+        // Build intent for notification content
+        Intent viewIntent = new Intent(this, MainActivity.class);
+        viewIntent.putExtra(EXTRA_EVENT_ID, 002);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        // Notification channel ID is ignored for Android 7.1.1
+        // (API level 25) and lower.
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, id)
+                        .setSmallIcon(R.drawable.ic_message)
+                        .setContentTitle("Alert!!!")
+                        .setContentText("You have not reached the specific target!")
+                        .setContentIntent(viewPendingIntent);
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        // Issue the notification with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+
 }
 
