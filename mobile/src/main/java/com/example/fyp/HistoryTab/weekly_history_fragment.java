063f225d9bb3d8d5a702666abb747f6ac9102200
joderson <57339690+joderson@users.noreply.github.com>
2020-05-12 18:27:03 +0800

    History (updated)

--- mobile/src/main/java/com/example/fyp/HistoryTab/weekly_history_fragment.java
+++ mobile/src/main/java/com/example/fyp/HistoryTab/weekly_history_fragment.java
@@ -326,7 +326,7 @@ public class weekly_history_fragment extends Fragment implements DatePickerDialo
                     if (TotalHoursDuration>1 || TotalHoursDuration==1){
                         TotalMinutesDuration = (totalduration % 3600) / 60;
                         TotalSecondsDuration = totalduration % 60;
-                        totaldurationText.setText("Total duration: "+String.format("%02d", TotalHoursDuration)+ ":" + String.format("%02d", TotalMinutesDuration) + ":" + String.format("%02d", TotalSecondsDuration)  + " (hh:mm:ss)");
+                        totaldurationText.setText("Total duration: "+TotalHoursDuration+ ":" + String.format("%02d", TotalMinutesDuration) + ":" + String.format("%02d", TotalSecondsDuration)  + " (hh:mm:ss)");
                     }
                     else {
                         TotalMinutesDuration = (totalduration % 3600) / 60;
