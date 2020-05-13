b5aca1c2b1e9ea30dccd51901cc46f9cb5b40bfd
joderson <57339690+joderson@users.noreply.github.com>
2020-05-13 17:03:41 +0800

    Maps (updatedd)

--- mobile/src/main/java/com/example/fyp/Interface/MapsActivity.java
+++ mobile/src/main/java/com/example/fyp/Interface/MapsActivity.java
@@ -74,7 +74,7 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback,
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.fragment_map_2, container, false);
+        View v= inflater.inflate(R.layout.fragment_map, container, false);
 
 
         mapView = (MapView) v.findViewById(R.id.map);
