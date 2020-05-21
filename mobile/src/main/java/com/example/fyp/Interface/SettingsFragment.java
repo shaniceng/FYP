6072d7936b8983f068bd50c3f63490aba6f52659
shaniceNg <ngshanice1@yahoo.com.sg>
2020-05-21 08:59:20 +0800

    Badges(updated)

--- mobile/src/main/java/com/example/fyp/Interface/SettingsFragment.java
+++ mobile/src/main/java/com/example/fyp/Interface/SettingsFragment.java
@@ -118,7 +118,7 @@ public class SettingsFragment extends Fragment {
                 weight.setText(userProfile.getUserWeight() +" kg");
                 birthday.setText(userProfile.getUserBirthday());
                 radioButton.setText(userProfile.getRadiotext());
-                age.setText(String.valueOf(prefs.getInt("YOUR COUNTER PREF KEY", 0)));
+//                age.setText(String.valueOf(prefs.getInt("YOUR COUNTER PREF KEY", 0)));
 //                age.setText(String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_YEAR)));
             }
 
